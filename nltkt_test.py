b3dc33a56af005203912a20a34c65a6801646a20
Gvvs <anjohn@gmx.com>
2023-09-10 10:30:35 +0800

    changed

--- nltkt_test.py
+++
@@ -1,8 +0,0 @@
-"""NLTK Test"""
-import nltk
-
-sentence = """Over the centuries, the castle walls have crumbled."""
-
-tokens = nltk.word_tokenize(sentence)
-print(tokens)
-print(nltk.pos_tag(tokens))
